15832d4406f7c34277a7923a3fe3fb2bc1f04961
Jam <jam.hsu@lativ.com.tw>
2025-07-16 11:34:06 +0800

    Force in main actor

--- ImageSlideshow/Classes/InputSources/KingfisherSource.swift
+++ ImageSlideshow/Classes/InputSources/KingfisherSource.swift
@@ -55,6 +55,7 @@ public class KingfisherSource: NSObject, InputSource {
     ///   - imageView: UIImageView that receives the loaded image
     ///   - callback: Completion callback with an optional image
     @objc
+    @MainActor
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
         imageView.kf.setImage(with: self.url, placeholder: self.placeholder, options: self.options, progressBlock: nil) { result in
             switch result {
@@ -80,6 +81,7 @@ public class KingfisherSource: NSObject, InputSource {
     /// Cancel an image download task
     ///
     /// - Parameter imageView: UIImage view with the download task that should be canceled
+    @MainActor
     public func cancelLoad(on imageView: UIImageView) {
         imageView.kf.cancelDownloadTask()
     }
